26826796933b4c6cd320a74f25df6ef04932d9a9
Nikolay Botev <bono8106@yahoo.com>
2011-12-31 01:01:30 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CSharpAsyncBasic
+{
+    class Program
+    {
+        static void log(Object message)
+        {
+            Console.WriteLine("[" + Thread.CurrentThread.ManagedThreadId + "] " + message);
+        }
+
+        static void Main(string[] args)
+        {
+            log("Main enter");
+            MainTest();
+            log("Main leave");
+            var xx = test(42);
+            xx.ContinueWith((x) =>
+            {
+                log("got result " + x.Result);
+            });
+            log("main done");
+            Console.ReadLine();
+        }
+
+        static async void MainTest()
+        {
+            log("Hello world");
+            for (int i = 0; i < 5; i++)
+            {
+                var y = await test(i);
+                log("Hello result " + i + " = " + y);
+            }
+        }
+
+        static async Task<int> test(int i)
+        {
+            var x = Task.Run(() =>
+            {
+                log("computing " + i);
+                return i * i;
+            });
+            return await x;
+        }
+
+        static Task<int> test2(int x)
+        {
+            return Task.FromResult(x + x);
+        }
+    }
+}
